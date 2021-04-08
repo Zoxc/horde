2005883c62d7a328bad83d49cbd0980610866adf
John Kåre Alsaker <john.kare.alsaker@gmail.com>
2021-04-08 20:33:40 +0200

    extract raw from hashbrown

---
+++ src/lib.rs
@@ -0,0 +1,21 @@
+extern crate alloc;
+
+#[macro_use]
+mod macros;
+
+mod scopeguard;
+pub mod raw;
+
+/// The error type for `try_reserve` methods.
+#[derive(Clone, PartialEq, Eq, Debug)]
+pub enum TryReserveError {
+    /// Error due to the computed capacity exceeding the collection's maximum
+    /// (usually `isize::MAX` bytes).
+    CapacityOverflow,
+
+    /// The memory allocator returned an error
+    AllocError {
+        /// The layout of the allocation request that failed.
+        layout: alloc::alloc::Layout,
+    },
+}
