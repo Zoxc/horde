0bd6317b51412aee21384b7848150ebea476afaf
John Kåre Alsaker <john.kare.alsaker@gmail.com>
2021-04-28 18:28:28 +0200

    fix

--- src/sync_insert_table.rs
+++ src/sync_insert_table.rs
@@ -963,11 +963,6 @@ impl<'a, T: Send + Clone, S> Write<'a, T, S> {
 
         let buckets = capacity_to_buckets(new_capacity).expect("capacity overflow");
 
-        // Have at least the number of buckets required to fill a group.
-        // This avoids logic to deal with control bytes not associated with a bucket
-        // when batch processing a group.
-        let buckets = usize::max(buckets, Group::WIDTH);
-
         let new_table = unsafe { table.clone(&self.table.hash_builder, buckets, hasher) };
 
         self.replace_table(new_table);
@@ -1300,23 +1295,32 @@ fn capacity_to_buckets(cap: usize) -> Option<usize> {
 
     // For small tables we require at least 1 empty bucket so that lookups are
     // guaranteed to terminate if an element doesn't exist in the table.
-    if cap < 8 {
+    let result = if cap < 8 {
         // We don't bother with a table size of 2 buckets since that can only
         // hold a single element. Instead we skip directly to a 4 bucket table
         // which can hold 3 elements.
-        return Some(if cap < 4 { 4 } else { 8 });
-    }
+        if cap < 4 {
+            4
+        } else {
+            8
+        }
+    } else {
+        // Otherwise require 1/8 buckets to be empty (87.5% load)
+        //
+        // Be careful when modifying this, calculate_layout relies on the
+        // overflow check here.
+        let adjusted_cap = cap.checked_mul(8)? / 7;
 
-    // Otherwise require 1/8 buckets to be empty (87.5% load)
-    //
-    // Be careful when modifying this, calculate_layout relies on the
-    // overflow check here.
-    let adjusted_cap = cap.checked_mul(8)? / 7;
+        // Any overflows will have been caught by the checked_mul. Also, any
+        // rounding errors from the division above will be cleaned up by
+        // next_power_of_two (which can't overflow because of the previous divison).
+        adjusted_cap.next_power_of_two()
+    };
 
-    // Any overflows will have been caught by the checked_mul. Also, any
-    // rounding errors from the division above will be cleaned up by
-    // next_power_of_two (which can't overflow because of the previous divison).
-    Some(adjusted_cap.next_power_of_two())
+    // Have at least the number of buckets required to fill a group.
+    // This avoids logic to deal with control bytes not associated with a bucket
+    // when batch processing a group.
+    Some(usize::max(result, Group::WIDTH))
 }
 
 /// Primary hash function, used to select the initial bucket to probe from.
