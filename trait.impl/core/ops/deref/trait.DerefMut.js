e1d59ff1ddea97e3dfc31d5d64fc87c1a21bb159
Zoxc <Zoxc@users.noreply.github.com>
2026-04-29 09:00:41 +0000

    deploy: d4621c89ad3aa52f99227ee9680f3a2a92f5da15

---
+++ trait.impl/core/ops/deref/trait.DerefMut.js
@@ -0,0 +1,9 @@
+(function() {
+    const implementors = Object.fromEntries([["horde",[["impl&lt;'a, K, V, S&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/deref/trait.DerefMut.html\" title=\"trait core::ops::deref::DerefMut\">DerefMut</a> for <a class=\"struct\" href=\"horde/sync_table/struct.LockedWrite.html\" title=\"struct horde::sync_table::LockedWrite\">LockedWrite</a>&lt;'a, K, V, S&gt;",0],["impl&lt;'a, T&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/deref/trait.DerefMut.html\" title=\"trait core::ops::deref::DerefMut\">DerefMut</a> for <a class=\"struct\" href=\"horde/sync_push_vec/struct.LockedWrite.html\" title=\"struct horde::sync_push_vec::LockedWrite\">LockedWrite</a>&lt;'a, T&gt;",0]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":59,"fragment_lengths":[685]}
